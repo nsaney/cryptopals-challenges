a5f8111e8fcafd4c4b8dd4f05bca71e3ec02cd3e
Nicholas Saney <nsaney@gmail.com>
2020-06-06 15:53:49 -0400

    Moved around some things from S1C1.

--- src/main/java/chairosoft/cryptopals/Common.java
+++ src/main/java/chairosoft/cryptopals/Common.java
@@ -12,4 +12,30 @@ public final class Common {
     ////// Constants //////
     public static final Charset COMMON_CHARSET = StandardCharsets.UTF_8;
     
+    
+    ////// Static Methods //////
+    public static byte[] fromHex(String hexText) {
+        int textLen = hexText.length();
+        int dataLen = textLen / 2;
+        byte[] data = new byte[dataLen];
+        for (int i = 0, j = 0; j < dataLen; i += 2, ++j) {
+            String valueText = hexText.substring(i, i + 2);
+            int valueDataInt = Integer.parseInt(valueText, 16);
+            byte valueData = (byte)valueDataInt;
+            data[j] = valueData;
+        }
+        return data;
+    }
+    
+    public static String toHex(byte[] data) {
+        int dataLen = data.length;
+        int textLen = dataLen * 2;
+        StringBuilder sb = new StringBuilder(textLen);
+        for (byte valueData : data) {
+            String valueText = Integer.toString(valueData, 16);
+            sb.append(valueText);
+        }
+        return sb.toString();
+    }
+    
 }
--- src/main/java/chairosoft/cryptopals/Set01Challenge01.java
+++ src/main/java/chairosoft/cryptopals/Set01Challenge01.java
@@ -2,6 +2,8 @@ package chairosoft.cryptopals;
 
 import java.util.Base64;
 
+import static chairosoft.cryptopals.Common.fromHex;
+
 /**
  * https://cryptopals.com/sets/1/challenges/1
  */
@@ -10,22 +12,9 @@ public class Set01Challenge01 {
     ////// Main Method //////
     public static void main(String... args) throws Exception {
         String input = args[0];
-        byte[] data = parseHex(input);
+        byte[] data = fromHex(input);
         byte[] dataBase64 = Base64.getEncoder().encode(data);
         System.out.write(dataBase64);
-    }
-    
-    public static byte[] parseHex(String hexText) {
-        int textLen = hexText.length();
-        int dataLen = textLen / 2;
-        byte[] data = new byte[dataLen];
-        for (int i = 0, j = 0; j < dataLen; i += 2, ++j) {
-            String valueText = hexText.substring(i, i + 2);
-            int valueDataInt = Integer.parseInt(valueText, 16);
-            byte valueData = (byte)valueDataInt;
-            data[j] = valueData;
-        }
-        return data;
     }
     
 }
