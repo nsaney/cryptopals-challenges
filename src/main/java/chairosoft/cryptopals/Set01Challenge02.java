ed1f4c23028a5ffe23637367cabaf31be6011b6d
Nicholas Saney <nsaney@gmail.com>
2020-06-06 16:03:37 -0400

    Completed S1C2.

---
+++ src/main/java/chairosoft/cryptopals/Set01Challenge02.java
@@ -0,0 +1,35 @@
+package chairosoft.cryptopals;
+
+import java.util.Arrays;
+
+import static chairosoft.cryptopals.Common.fromHex;
+import static chairosoft.cryptopals.Common.toHex;
+
+/**
+ * https://cryptopals.com/sets/1/challenges/2
+ */
+public class Set01Challenge02 {
+    
+    ////// Main Method //////
+    public static void main(String... args) throws Exception {
+        byte[] x = fromHex(args[0]);
+        byte[] y = fromHex(args[1]);
+        byte[] data = xor(x, y);
+        String dataHex = toHex(data);
+        System.out.print(dataHex);
+    }
+    
+    public static byte[] xor(byte[] x, byte[] y) {
+        int maxLen = Math.max(x.length, y.length);
+        byte[] xAdjusted = x.length == maxLen ? x : Arrays.copyOfRange(x, 0, maxLen);
+        byte[] yAdjusted = y.length == maxLen ? y : Arrays.copyOfRange(y, 0, maxLen);
+        byte[] result = new byte[maxLen];
+        for (int i = 0; i < maxLen; ++i) {
+            byte xVal = xAdjusted[i];
+            byte yVal = yAdjusted[i];
+            result[i] = (byte)((xVal ^ yVal) & 0xff);
+        }
+        return result;
+    }
+    
+}
---
+++ src/test/java/chairosoft/cryptopals/Set01Challenge02Test.java
@@ -0,0 +1,30 @@
+package chairosoft.cryptopals;
+
+import org.junit.Test;
+
+import static chairosoft.cryptopals.Common.*;
+import static chairosoft.cryptopals.TestUtils.getStdOut;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * https://cryptopals.com/sets/1/challenges/2
+ */
+public class Set01Challenge02Test {
+    
+    ////// Instance Methods - Tests //////
+    @Test
+    public void testSet01Challenge01() throws Exception {
+        String x = "1c0111001f010100061a024b53535009181c";
+        String y = "686974207468652062756c6c277320657965";
+        String expectedOutput = "746865206b696420646f6e277420706c6179";
+        byte[] actualOutputBytes = getStdOut(Set01Challenge02::main, x, y);
+        String actualOutput = new String(actualOutputBytes, COMMON_CHARSET);
+        assertThat(actualOutput, equalTo(expectedOutput));
+        String xText = parseFromHex(x);
+        String yText = parseFromHex(y);
+        System.out.printf("S1C2 x:   %s\n", xText);
+        System.out.printf("S1C2 y:   %s\n", yText);
+        System.out.printf("S1C2 out: %s\n", actualOutput);
+    }
+}
