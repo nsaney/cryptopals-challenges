ec70ecc51078d3c1fb5efd14e6638aad7208c43a
Saney, Nicholas <Nicholas.Saney@gtri.gatech.edu>
2020-06-16 10:50:55 -0400

    Finished S2C9.

---
+++ src/main/java/chairosoft/cryptopals/Set02Challenge09.java
@@ -0,0 +1,35 @@
+package chairosoft.cryptopals;
+
+import java.util.Arrays;
+
+import static chairosoft.cryptopals.Common.*;
+
+/**
+ * https://cryptopals.com/sets/2/challenges/9
+ */
+public class Set02Challenge09 {
+    
+    ////// Main Method //////
+    public static void main(String... args) throws Exception {
+        byte[] input = args[0].getBytes(COMMON_CHARSET);
+        int blockSize = Integer.parseInt(args[1]);
+        byte[] output = applyPkcs7(blockSize, input);
+        String outputText = toDisplayableText(output);
+        System.out.println(outputText);
+    }
+    
+    
+    ////// Static Methods //////
+    public static byte[] applyPkcs7(int blockSize, byte... data) {
+        int remainder = data.length % blockSize;
+        int paddingLength = blockSize - remainder;
+        byte paddingByte = (byte)(paddingLength & 0xff);
+        int updatedLength = data.length + paddingLength;
+        byte[] result = Arrays.copyOf(data, updatedLength);
+        for (int i = data.length; i < updatedLength; ++i) {
+            result[i] = paddingByte;
+        }
+        return result;
+    }
+    
+}
---
+++ src/test/java/chairosoft/cryptopals/Set02Challenge09Test.java
@@ -0,0 +1,25 @@
+package chairosoft.cryptopals;
+
+import org.junit.Test;
+
+import static chairosoft.cryptopals.Common.COMMON_CHARSET;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * https://cryptopals.com/sets/2/challenges/9
+ */
+public class Set02Challenge09Test extends TestBase {
+    
+    ////// Instance Methods - Tests //////
+    @Test
+    public void doTest() throws Exception {
+        String inputText = "YELLOW SUBMARINE";
+        int blockSize = 20;
+        String expectedOutputText = "YELLOW SUBMARINE\\x[04040404]\n";
+        byte[] actualOutput = getStdOut(Set02Challenge09::main, inputText, "" + blockSize);
+        String actualOutputText = new String(actualOutput, COMMON_CHARSET);
+        assertThat("Output Text", actualOutputText, equalTo(expectedOutputText));
+    }
+    
+}
