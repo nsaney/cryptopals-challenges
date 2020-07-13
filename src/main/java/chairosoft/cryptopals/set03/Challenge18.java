62de8fe5b7597fb04dbf98cbbae237d847480750
Nicholas Saney <nsaney@gmail.com>
2020-07-12 23:02:43 -0400

    Started on S3C18.

---
+++ src/main/java/chairosoft/cryptopals/set03/Challenge18.java
@@ -0,0 +1,20 @@
+package chairosoft.cryptopals.set03;
+
+
+import static chairosoft.cryptopals.Common.fromBase64Text;
+
+/**
+ * https://cryptopals.com/sets/3/challenges/18
+ */
+public class Challenge18 {
+    
+    ////// Main Method //////
+    public static void main(String... args) throws Exception {
+        byte[] key = fromBase64Text(args[0]);
+    }
+    
+    
+    ////// Static Methods //////
+    // TODO
+    
+}
---
+++ src/test/java/chairosoft/cryptopals/set03/Challenge18Test.java
@@ -0,0 +1,20 @@
+package chairosoft.cryptopals.set03;
+
+import chairosoft.cryptopals.TestBase;
+import org.junit.Test;
+
+import static chairosoft.cryptopals.Common.*;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * https://cryptopals.com/sets/3/challenges/18
+ */
+public class Challenge18Test extends TestBase {
+    
+    ////// Instance Methods - Tests //////
+    @Test
+    public void doTest() throws Exception {
+        throw new UnsupportedOperationException("No test here yet!");
+    }
+    
+}
