5112fed53390e0918d0d4da3fe4a2331f6d24e9c
Nicholas Saney <nsaney@gmail.com>
2020-07-27 12:38:28 -0400

    Started S3C21.

---
+++ src/main/java/chairosoft/cryptopals/set03/Challenge21.java
@@ -0,0 +1,32 @@
+package chairosoft.cryptopals.set03;
+
+import chairosoft.cryptopals.set01.Challenge03;
+import chairosoft.cryptopals.set01.Challenge06;
+
+import java.io.File;
+import java.nio.ByteOrder;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static chairosoft.cryptopals.Common.*;
+
+/**
+ * https://cryptopals.com/sets/3/challenges/21
+ */
+public class Challenge21 {
+    
+    ////// Main Method //////
+    public static void main(String... args) throws Exception {
+        throw new UnsupportedOperationException("");
+    }
+    
+    
+    ////// Static Inner Classes //////
+    
+    
+    ////// Static Methods //////
+}
---
+++ src/test/java/chairosoft/cryptopals/set03/Challenge21Test.java
@@ -0,0 +1,21 @@
+package chairosoft.cryptopals.set03;
+
+import chairosoft.cryptopals.TestBase;
+import org.junit.Test;
+
+import java.io.File;
+
+import static chairosoft.cryptopals.Common.randomBytes;
+
+/**
+ * https://cryptopals.com/sets/3/challenges/21
+ */
+public class Challenge21Test extends TestBase {
+    
+    ////// Instance Methods - Tests //////
+    @Test
+    public void doTest() throws Exception {
+        // no test here yet
+    }
+    
+}
