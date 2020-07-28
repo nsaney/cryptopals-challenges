a7ad026624cf669a6bebf2e30804eea8856b62b6
Nicholas Saney <nsaney@gmail.com>
2020-07-28 18:47:30 -0400

    Starting S3C22.

---
+++ src/main/java/chairosoft/cryptopals/set03/Challenge22.java
@@ -0,0 +1,16 @@
+package chairosoft.cryptopals.set03;
+
+/**
+ * https://cryptopals.com/sets/3/challenges/22
+ */
+public class Challenge22 {
+    
+    ////// Main Method //////
+    public static void main(String... args) throws Exception {
+        //
+    }
+    
+    
+    ////// Static Methods //////
+    //
+}
---
+++ src/test/java/chairosoft/cryptopals/set03/Challenge22Test.java
@@ -0,0 +1,21 @@
+package chairosoft.cryptopals.set03;
+
+import chairosoft.cryptopals.TestBase;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
+/**
+ * https://cryptopals.com/sets/3/challenges/22
+ */
+public class Challenge22Test extends TestBase {
+    
+    ////// Instance Methods - Tests //////
+    @Test
+    public void doTest() throws Exception {
+        //
+    }
+    
+}
