ca26b52e398212852bdb699e06873c8f0036fe70
Nicholas Saney <nsaney@gmail.com>
2020-07-28 19:43:34 -0400

    Finished S3C22.

--- src/main/java/chairosoft/cryptopals/set03/Challenge22.java
+++ src/main/java/chairosoft/cryptopals/set03/Challenge22.java
@@ -1,5 +1,7 @@
 package chairosoft.cryptopals.set03;
 
+import chairosoft.cryptopals.set03.Challenge21.MT19937Random;
+
 /**
  * https://cryptopals.com/sets/3/challenges/22
  */
@@ -7,10 +9,46 @@ public class Challenge22 {
     
     ////// Main Method //////
     public static void main(String... args) throws Exception {
-        //
+        long currentTimeSeconds = System.currentTimeMillis() / 1000;
+        long firstValue = Long.parseLong(args[0]);
+        int minAdjustment = Integer.parseInt(args[1]);
+        int maxAdjustment = Integer.parseInt(args[2]);
+        long seed = crackMt19937TimeBasedSeed(currentTimeSeconds, firstValue, minAdjustment, maxAdjustment);
+        System.out.println(seed);
     }
     
     
     ////// Static Methods //////
-    //
+    public static long crackMt19937TimeBasedSeed(
+        long callTimeSeconds,
+        long firstValue,
+        int minAdjustment,
+        int maxAdjustment
+    )
+        throws Exception
+    {
+        int range = maxAdjustment - minAdjustment;
+        int extendedMaxAdjustment = maxAdjustment + range;
+        for (int i = minAdjustment; i < extendedMaxAdjustment; ++i) {
+            long potentialSeed = callTimeSeconds - i;
+            long potentialFirstValue = firstValueForMt19937Seed(potentialSeed);
+            if (firstValue == potentialFirstValue) {
+                return potentialSeed;
+            }
+        }
+        throw new RuntimeException(String.format(
+            "Unable to crack seed from value [%s] in range from [%s - %s] to [%s - %s].",
+            firstValue,
+            callTimeSeconds,
+            extendedMaxAdjustment,
+            callTimeSeconds,
+            minAdjustment
+        ));
+    }
+    
+    public static long firstValueForMt19937Seed(long seed) {
+        MT19937Random mt19937Random = new MT19937Random(seed);
+        return mt19937Random.extractNumber();
+    }
+    
 }
--- src/test/java/chairosoft/cryptopals/set03/Challenge22Test.java
+++ src/test/java/chairosoft/cryptopals/set03/Challenge22Test.java
@@ -1,11 +1,10 @@
 package chairosoft.cryptopals.set03;
 
 import chairosoft.cryptopals.TestBase;
+import chairosoft.cryptopals.set03.Challenge21.MT19937Random;
 import org.junit.Test;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.util.List;
+import java.util.Random;
 
 /**
  * https://cryptopals.com/sets/3/challenges/22
@@ -15,7 +14,23 @@ public class Challenge22Test extends TestBase {
     ////// Instance Methods - Tests //////
     @Test
     public void doTest() throws Exception {
-        //
+        Random rand = new Random();
+        long currentTimeSeconds = System.currentTimeMillis() / 1000;
+        int minAdjustment = 40;
+        int maxAdjustment = 1000;
+        long adjustment = rand.nextInt(maxAdjustment - minAdjustment) + minAdjustment;
+        long seed = currentTimeSeconds - adjustment;
+        MT19937Random mt19937Random = new MT19937Random(seed);
+        long firstValue = mt19937Random.extractNumber();
+        String expectedResultText = String.valueOf(seed);
+        System.err.printf("%s was the seed; calling with first value [%s].\n", seed, firstValue);
+        assertResultOutputEquals(
+            expectedResultText,
+            Challenge22::main,
+            firstValue,
+            minAdjustment,
+            maxAdjustment
+        );
     }
     
 }
